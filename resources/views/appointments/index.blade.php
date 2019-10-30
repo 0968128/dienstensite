16ab9268d6787d5272f1ab768779d2f8621d1130
Lucas Hijman <46541848+lucashijman@users.noreply.github.com>
2019-10-30 10:55:26 +0100

    update index afsprakenoverzicht
    Geen waarden meer voor opties uit de dropdownlist. Deze bepaal ik later.

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -2,9 +2,9 @@
 <form name="searchfilter" id="searchfilter" action="">
     <input type="text" name="searchbar" placeholder="Typ een zoekterm...">
         <select type="text" name="filter">
-            <option value="alles">Alle afspraken</option>
-            <option value="alsklant">Ik als klant</option>
-            <option value="alsdienstverlener">Ik als dienstverlener</option>
+            <option value="">Alle afspraken</option>
+            <option value="">Ik als klant</option>
+            <option value="">Ik als dienstverlener</option>
         </select>
     <input type="submit" name="submit" value="zoeken">
 </form>

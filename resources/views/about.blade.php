0a587191067e250276f19e95d3584ca613437cce
Lucas <096128@hr.nl>
2020-01-13 14:37:36 +0100

    about
    ik geef wat meer uitleg over de bedoeling van deze site

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,5 +5,15 @@
 @endsection
 
 @section('content')
-    <p>Ik ben ben Lucas, 18 jaar en ik maak een website waar vraag en aanbod van diensten elkaar ontmoeten.</p>
+    <h3>
+        Ik ben ben Lucas, 18 jaar en ik maak een website waar vraag en aanbod van diensten elkaar ontmoeten.
+    </h3>
+    <p>
+        Wat heb je precies aan mijn site? Op mijn site kun je in contact komen met mensen die uiteenlopende expertises hebben. 
+        Je wil bijvoorbeeld dat je computer opgeschoond wordt, maar je hebt geen flauw idee de meeste programma's doen. Je kunt
+        dan bij een computersysteemexpert een conceptafspraak maken door te klikken op 'afspraak maken'. Je geeft de afspraak
+        een naam, beschrijft het probleem dat je ervaart en je kiest de dienstverlenende gebruiker. Deze gebruiker krijgt de
+        afspraak ook te zien in zijn scherm. Hij/zij contacteert u terug via mail, sms of een ander communicatief platform. Via
+        de mail moeten jullie het vervolgens eens worden over een tijd en een prijs.
+    </p>
 @endsection

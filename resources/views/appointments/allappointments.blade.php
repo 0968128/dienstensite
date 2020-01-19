33f2d55673a978afbc53fe4fa5ddf1266c6a4fda
Lucas <096128@hr.nl>
2020-01-19 18:58:59 +0100

    alle afspraken overzicht
    poging om eager loading toe te passen met bij het ophalen van alle afspraken met gebruikers

--- resources/views/appointments/allappointments.blade.php
+++ resources/views/appointments/allappointments.blade.php
@@ -4,7 +4,7 @@
     <h1>Alle afspraken van de website</h1>
     <ul>
         @foreach ($appointments as $appointment)
-            <div href="/appointments/{{ $appointment->id }}">{{ $appointment->name }}{{  }}{{  }}</div>
+            <div href="/appointments/{{ $appointment->id }}">{{ $appointment->name }}{{ $appointment->klant_id }}{{ $appointment->dienstverlener_id }}</div>
         @endforeach
     </ul>
 @endsection

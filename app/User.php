cab38e36110112d82c5a55627b7e52fdc2b2a8ac
Lucas <096128@hr.nl>
2020-01-19 23:50:07 +0100

    relatie vernieuwde aanroep
    tussen aanhalingstekens is de oude manier ofzo

--- app/User.php
+++ app/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
     ];
 
     public function roles() {
-        return $this->belongsToMany('App\Role');
+        return $this->belongsToMany(Role::class);
     }
 
     public function hasRole($role) {

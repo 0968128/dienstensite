5fba75b3d15b3f1f10ec2ec31024d40213893f16
Lucas <096128@hr.nl>
2020-01-19 23:49:24 +0100

    alle appointments
    pagina werkt, maar niet met eager loading

--- resources/views/appointments/allappointments.blade.php
+++ resources/views/appointments/allappointments.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <h1>Alle afspraken van de website</h1>
     @foreach ($appointments as $appointment)
-        <div>{{ $appointment->user->name }} heeft deze dienst aangevraagd: {{ $appointment->name }}</div>
+        <div>{{ $appointment->user }} heeft deze dienst aangevraagd: {{ $appointment->name }}</div>
     @endforeach
 @endsection

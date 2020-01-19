b173fcda60dc65749463537b05c7b4889a242876
Lucas <096128@hr.nl>
2020-01-19 22:40:22 +0100

    filter
    filter laten staan op de waarde waarop hij stond voor het verzenden van de form

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -3,11 +3,21 @@
 @section('content')
     <h1>Afspraken</h1>
     <form name="searchfilter" id="searchfilter" action="">
-        <input type="text" name="searchbar" placeholder="Typ een zoekterm...">
+        <input type="text" name="searchbar" placeholder="Typ een zoekterm..." value="{{ $searchbar }}">
         <select type="text" name="filter">
-            <option selected value="">Kies</option>
-            <option value="2">Ik als klant</option>
-            <option value="3">Ik als dienstverlener</option>
+            @if($filter == 3)
+                <option value="">Kies</option>
+                <option value="2">Ik als klant</option>
+                <option selected value="3">Ik als dienstverlener</option>
+            @elseif ($filter == 2)
+                <option value="">Kies</option>
+                <option selected value="2">Ik als klant</option>
+                <option value="3">Ik als dienstverlener</option>
+            @else
+                <option selected value="">Kies</option>
+                <option value="2">Ik als klant</option>
+                <option value="3">Ik als dienstverlener</option>
+            @endif
         </select>
         <input type="submit" name="submit" value="zoeken">
     </form>

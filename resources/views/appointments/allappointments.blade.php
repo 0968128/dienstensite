a5f003f508d9bade2595ac3d20a9d0bc174fd80a
Lucas <096128@hr.nl>
2020-01-22 11:20:46 +0100

    view alle afspraken
    de view voor alle afspraken van de website is weg

--- resources/views/appointments/allappointments.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Alle afspraken van de website</h1>
-    @foreach ($appointments as $appointment)
-        <div>{{ $appointment->user }} heeft deze dienst aangevraagd: {{ $appointment->name }}</div>
-    @endforeach
-@endsection

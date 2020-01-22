b308bccb2d787b33761f0a5e44e876e3ea8b1b71
Lucas <096128@hr.nl>
2020-01-22 11:19:12 +0100

    user relatie
    user relatie is niet meer nodig nu ik hiermee geen eager loading meer doe

--- app/Appointment.php
+++ app/Appointment.php
@@ -7,7 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model {
     protected $table = 'appointments';
     protected $guarded = [];
-    public function user() {
-        return $this->belongsTo(User::class);
-    }
 }
